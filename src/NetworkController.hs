2938506454fd154d5da8f7ac7b85cc6cff335361
Santiago Ocamica <santi6982@gmail.com>
2020-01-18 10:21:24 -0500

    NetworkController: Add state for readiness and waitForPlayers function.

--- src/NetworkController.hs
+++ src/NetworkController.hs
@@ -1,8 +1,10 @@
 module NetworkController (
+  main,
   start,
   broadcast,
   send,
   receive,
+  waitForPlayers,
   NetworkController,
   Player(Player1,Player2)
   ) where
@@ -15,32 +17,49 @@ import Network.Socket (
   socket,
   socketToHandle,
   setSocketOption,
+  PortNumber,
   Socket,
   Family(AF_INET),
   SocketType(Stream),
   SocketOption(ReuseAddr),
   SockAddr(SockAddrInet))
-
 import System.IO (
   hSetBuffering,
   hClose,
   hGetLine,
   hPutStrLn,
   IOMode(ReadWriteMode),
-  BufferMode(NoBuffering))
+  BufferMode(NoBuffering),
+  stdout)
 import Control.Exception
-import Control.Concurrent (newChan, readChan, writeChan, dupChan, forkIO, killThread, Chan)
+import Control.Concurrent (
+  newChan,
+  readChan,
+  writeChan,
+  dupChan,
+  forkIO,
+  killThread,
+  Chan,
+  MVar,
+  newMVar,
+  readMVar,
+  putMVar,
+  takeMVar,
+  swapMVar,
+  newEmptyMVar,
+  tryReadMVar,
+  isEmptyMVar
+  )
 import Control.Monad (when)
 import Control.Monad.Fix (fix)
 
-main = do 
-  controller <- start
+main = do
+  hSetBuffering stdout NoBuffering
+  controller <- start 4242
   fix $ \loop -> do
-    send controller Player1 "Welcome, you are player 1"
-    send controller Player2 "Welcome, you are player 2"
-    broadcast controller "All players ready"
     msg <- receive controller
     print msg
+    printNetworkControllerState $ state controller
     loop
 
 data Player = Player1 | Player2 deriving (Show)
@@ -50,45 +69,100 @@ type Msg = (Player, String)
 data NetworkController = NetworkController {
   player1 :: Chan String,
   player2 :: Chan String,
-  received :: Chan Msg
+  received :: Chan Msg,
+  state :: NetworkControllerState
 }
+
+data PlayerState = PlayerState {
+  ready :: MVar Bool
+}
+
+newPlayerState = do 
+  ready <- newEmptyMVar
+  return $ PlayerState ready
+
+printPlayerState (PlayerState ready) = do
+  isReady <- tryReadMVar ready
+  putStr $ "Ready: " ++ show isReady
+
+data NetworkControllerState = NetworkControllerState {
+  player1State :: PlayerState,
+  player2State :: PlayerState
+}
+
+newNetworkControllerState = do
+  player1State <- newPlayerState
+  player2State <- newPlayerState
+  return NetworkControllerState { player1State = player1State, player2State = player2State }
+
+getPlayerState Player1 = player1State
+getPlayerState Player2 = player2State
+
+printNetworkControllerState (NetworkControllerState player1 player2)  = do
+  putStrLn "State:"
+  putStr "\tPlayer1: { "
+  printPlayerState player1
+  putStr " }\n\tPlayer2: { "
+  printPlayerState player2
+  putStr " }\n"
+  
 
 newNetworkController :: IO NetworkController
 newNetworkController = do
   player1Chan <- newChan
   player2Chan <- newChan
   receiveChan <- newChan
-  return $ NetworkController player1Chan player2Chan receiveChan
+  state <- newNetworkControllerState
+  return $ NetworkController player1Chan player2Chan receiveChan state
 
 send :: NetworkController -> Player -> String -> IO ()
-send (NetworkController p1 _ _) Player1 msg = writeChan p1 msg
-send (NetworkController _ p2 _) Player2 msg = writeChan p2 msg
+send (NetworkController p1 _ _ _) Player1 msg = writeChan p1 msg
+send (NetworkController _ p2 _ _) Player2 msg = writeChan p2 msg
 
 handleSent :: NetworkController -> Player -> IO String
-handleSent (NetworkController p1 _ _) Player1 = readChan p1
-handleSent (NetworkController _ p2 _) Player2 = readChan p2
+handleSent (NetworkController p1 _ _ _) Player1 = readChan p1
+handleSent (NetworkController _ p2 _ _) Player2 = readChan p2
 
 receive :: NetworkController -> IO Msg
-receive (NetworkController _ _ rx) = readChan rx
+receive (NetworkController _ _ rx _) = readChan rx
 
 handleReceived :: NetworkController -> Player -> String -> IO ()
-handleReceived (NetworkController _ _ rx) player msg = writeChan rx (player, msg)
+handleReceived (NetworkController _ _ rx _) player msg = writeChan rx (player, msg)
 
 broadcast :: NetworkController -> String -> IO ()
-broadcast (NetworkController p1 p2 _) msg = do
+broadcast (NetworkController p1 p2 _ _) msg = do
   writeChan p1 msg
   writeChan p2 msg
 
-start :: IO NetworkController
-start = do
+setReady :: NetworkController -> Player -> Bool -> IO ()
+setReady controller player value = do
+  empty <- isEmptyMVar playerReadyMVar
+  if empty
+    then putMVar playerReadyMVar value
+    else do { swapMVar playerReadyMVar value; return () }
+  return ()
+  where playerReadyMVar = (ready $ getPlayerState player $ state $ controller)
+
+start :: PortNumber -> IO NetworkController
+start port = do
   sock <- socket AF_INET Stream 0
   setSocketOption sock ReuseAddr 1
-  bind sock (SockAddrInet 4242 0)
+  bind sock (SockAddrInet port 0)
   listen sock 2
-  print "Listening on port 4242"
+  print $ "Listening on port " ++ show port
   controller <- newNetworkController
   forkIO (mainLoop sock controller)
   return controller
+
+waitForPlayers :: NetworkController -> IO ()
+waitForPlayers controller = do
+  ready1 <- takeMVar player1ReadyMVar
+  ready2 <- takeMVar player2ReadyMVar
+  when ready1 (putMVar player2ReadyMVar ready1)
+  when ready2 (putMVar player1ReadyMVar ready2)
+  when (not ready1 || not ready2) (waitForPlayers controller)
+  where player1ReadyMVar = (ready $ player1State $ state $ controller)
+        player2ReadyMVar = (ready $ player2State $ state $ controller)
 
 mainLoop :: Socket -> NetworkController -> IO ()
 mainLoop sock controller = do
@@ -113,10 +187,7 @@ runConn sock controller player = do
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
 
-    hPutStrLn hdl "Hi, what's your name?"
-    name <- fmap init (hGetLine hdl)
-    broadcast controller ("--> " ++ name ++ " entered game.")
-    hPutStrLn hdl ("Welcome, " ++ name ++ "!")
+    setReady controller player True
 
     -- fork off a thread for reading from the transmission channel and forwarding
     writer <- forkIO $ fix $ \loop -> do
@@ -133,5 +204,6 @@ runConn sock controller player = do
              -- else, continue looping.
              _      -> handleReceived controller player msg >> loop
 
+    setReady controller player False
     killThread writer                      -- kill after the loop ends
     hClose hdl                             -- close the handle
